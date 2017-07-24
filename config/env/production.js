35090698e1ea7b37d9c96ab8a45edcf1e6365ef3
Logan Born <lcborn4@gmail.com>
2017-07-23 22:38:24 -0400

    removed the session connect-mongo

--- config/env/production.js
+++ config/env/production.js
@@ -22,10 +22,10 @@ module.exports = {
   models: {
     connection: 'productionMongo'
   },
-  session: {
-    adapter: 'connect-mongo',
-    url: process.env.MONGODB_URI
-  }
+  // session: {
+  //   adapter: 'connect-mongo',
+  //   url: process.env.MONGODB_URI
+  // }
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
