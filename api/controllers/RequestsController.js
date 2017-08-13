cbb12afc3d657ec1d2f6e8592816c16196c699ff
Logan Born <lcborn4@gmail.com>
2017-08-13 14:31:29 -0400

    added requests model and controller

---
+++ api/controllers/RequestsController.js
@@ -0,0 +1,11 @@
+/**
+ * RequestsController
+ *
+ * @description :: Server-side logic for managing Requests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
