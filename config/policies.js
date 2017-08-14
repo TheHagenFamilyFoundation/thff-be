bff2008b8b11e61dfd371966ae357bbbced8e7f4
Logan Born <lcborn4@gmail.com>
2017-08-13 23:34:13 -0400

    added in the requests controller - no policy

--- config/policies.js
+++ config/policies.js
@@ -50,15 +50,18 @@ module.exports.policies = {
   // }
 
   '*': ['isAuthorized'], // Everything resctricted here
-  
+
   'UserController': {
     'create': true // We dont need authorization here, allowing public access
   },
 
   'AuthController': {
     '*': true // We dont need authorization here, allowing public access
-  }
+  },
 
+  'RequestsController': {
+    '*': true // We dont need authorization here, allowing public access
+  }
 
 
 };
