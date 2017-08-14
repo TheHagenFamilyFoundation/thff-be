40a4516e72d5e9c25d712f13ee9fa134906d59d0
Logan Born <lcborn4@gmail.com>
2017-08-13 22:12:46 -0400

    added the attributes for the user model

--- api/models/User.js
+++ api/models/User.js
@@ -1,14 +1,64 @@
 /**
- * User.js
+ * Users.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @docs        :: http://sailsjs.org/#!documentation/models
  */
+
+// We don't want to store password with out encryption
+var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
 
-  attributes: {
+  schema: true,
 
+  attributes: {
+    email: {
+      type: 'email',
+      required: 'true',
+      unique: true // Yes unique one
+    },
+    username: {
+      type: 'string',
+      required: 'true',
+      unique: true
+    },
+
+    encryptedPassword: {
+      type: 'string'
+    },
+    // We don't wan't to send back encrypted password either
+    toJSON: function () {
+      var obj = this.toObject();
+      delete obj.encryptedPassword;
+      return obj;
+    }
+  },
+  // Here we encrypt password before creating a User
+  beforeCreate: function (values, next) {
+    //sails.log("beforeCreate")
+
+    bcrypt.genSalt(10, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(values.password, salt, null, function (err, hash) {
+        if (err) return next(err);
+
+        values.encryptedPassword = hash;
+        next();
+      })
+    })
+  },
+
+  comparePassword: function (password, user, cb) {
+    bcrypt.compare(password, user.encryptedPassword, function (err, match) {
+
+      if (err) cb(err);
+      if (match) {
+        cb(null, true);
+      } else {
+        cb(err);
+      }
+    })
   }
 };
-
