5a8135fc3d1525deace779b5f7db7f5ca529d2bf
Logan Born <lcborn4@gmail.com>
2017-08-13 23:34:06 -0400

    added in a debug statemtn for index function

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,33 +6,36 @@
  */
 
 module.exports = {
-    index: function (req, res) {
-      var email = req.param('email');
-      var password = req.param('password');
-  
-      if (!email || !password) {
-        return res.json(401, {err: 'email and password required'});
-      }
-  
-      Users.findOne({email: email}, function (err, user) {
-        if (!user) {
-          return res.json(401, {err: 'invalid email or password'});
-        }
-  
-        Users.comparePassword(password, user, function (err, valid) {
-          if (err) {
-            return res.json(403, {err: 'forbidden'});
-          }
-  
-          if (!valid) {
-            return res.json(401, {err: 'invalid email or password'});
-          } else {
-            res.json({
-              user: user,
-              token: jwToken.issue({id : user.id })
-            });
-          }
-        });
-      })
+  index: function (req, res) {
+
+    sails.log("index")
+
+    var email = req.param('email');
+    var password = req.param('password');
+
+    if (!email || !password) {
+      return res.json(401, { err: 'email and password required' });
     }
-  };
+
+    Users.findOne({ email: email }, function (err, user) {
+      if (!user) {
+        return res.json(401, { err: 'invalid email or password' });
+      }
+
+      Users.comparePassword(password, user, function (err, valid) {
+        if (err) {
+          return res.json(403, { err: 'forbidden' });
+        }
+
+        if (!valid) {
+          return res.json(401, { err: 'invalid email or password' });
+        } else {
+          res.json({
+            user: user,
+            token: jwToken.issue({ id: user.id })
+          });
+        }
+      });
+    })
+  }
+};
