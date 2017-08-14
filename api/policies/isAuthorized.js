532a2779a137ed03b678714c5d525e46740b9ce1
Logan Born <lcborn4@gmail.com>
2017-08-13 22:12:55 -0400

    added the authorized policy

---
+++ api/policies/isAuthorized.js
@@ -0,0 +1,36 @@
+/**
+ * isAuthorized
+ *
+ * @description :: Policy to check if user is authorized with JSON web token
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Policies
+ */
+
+module.exports = function (req, res, next) {
+    var token;
+  
+    if (req.headers && req.headers.authorization) {
+      var parts = req.headers.authorization.split(' ');
+      if (parts.length == 2) {
+        var scheme = parts[0],
+          credentials = parts[1];
+  
+        if (/^Bearer$/i.test(scheme)) {
+          token = credentials;
+        }
+      } else {
+        return res.json(401, {err: 'Format is Authorization: Bearer [token]'});
+      }
+    } else if (req.param('token')) {
+      token = req.param('token');
+      // We delete the token from param to not mess with blueprints
+      delete req.query.token;
+    } else {
+      return res.json(401, {err: 'No Authorization header was found'});
+    }
+  
+    jwToken.verify(token, function (err, token) {
+      if (err) return res.json(401, {err: 'Invalid Token!'});
+      req.token = token; // This is the decrypted token or the payload you provided
+      next();
+    });
+  };
