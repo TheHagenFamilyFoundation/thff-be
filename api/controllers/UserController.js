868bcb426dc1d0ccfeeb8af878605d36423dcbdf
Logan Born <lcborn4@gmail.com>
2017-08-13 22:12:37 -0400

    aded the user.create function to userController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,10 +2,26 @@
  * UserController
  *
  * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
 module.exports = {
-	
-};
+    create: function (req, res) {
 
+        //sails.log("user create")
+
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.json(401, { err: 'Password doesn\'t match, What a shame!' });
+        }
+        User.create(req.body).exec(function (err, user) {
+            if (err) {
+                return res.json(err.status, { err: err });
+            }
+            // If user created successfuly we return user and token as response
+            if (user) {
+                // NOTE: payload is { id: user.id}
+                res.json(200, { user: user, token: jwToken.issue({ id: user.id }) });
+            }
+        });
+    }
+};
