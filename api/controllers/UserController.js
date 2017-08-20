06e25ce7f4e2534a8d18358a4becd3af56df0b7d
Logan Born <lcborn4@gmail.com>
2017-08-20 15:51:20 -0400

    created 2 new functions, UserNameExists, EmailExists

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -13,7 +13,15 @@ module.exports = {
         if (req.body.password !== req.body.confirmPassword) {
             return res.json(401, { err: 'Password doesn\'t match, What a shame!' });
         }
+
+
+        sails.log(req.body.username);
+
+
         User.create(req.body).exec(function (err, user) {
+
+            sails.log("User.create")
+
             if (err) {
                 return res.json(err.status, { err: err });
             }
@@ -24,4 +32,64 @@ module.exports = {
             }
         });
     }
+    ,
+    UserNameExists: function (req, res) {
+
+        sails.log(req.query.username);
+
+        User.find({
+            username: req.query.username
+        }).exec(function (err, userfound) {
+            if (err) {
+                return res.json(err.status, { err: err });
+            }
+
+            if (userfound) {
+                sails.log(userfound);
+
+                if (userfound.length > 0) {
+                    sails.log("user is found");
+                    return res.json({ userfound: true });
+                }
+                else {
+                    sails.log("user is not found");
+                    return res.json({ userfound: false });
+                }
+            }//end of userfound if
+
+
+
+        });
+
+    }//end of UserNameExists
+
+    ,
+    EmailExists: function (req, res) {
+
+        sails.log(req.query.email);
+
+        User.find({
+            email: req.query.email
+        }).exec(function (err, emailfound) {
+            if (err) {
+                return res.json(err.status, { err: err });
+            }
+
+            if (emailfound) {
+                sails.log(emailfound);
+
+                if (emailfound.length > 0) {
+                    sails.log("email is found");
+                    return res.json({ emailfound: true });
+                }
+                else {
+                    sails.log("email is not found");
+                    return res.json({ emailfound: false });
+                }
+            }//end of emailfound if
+
+        });
+
+    }
+
 };
