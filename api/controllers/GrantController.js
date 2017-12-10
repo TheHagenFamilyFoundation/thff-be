cd01a35440b227fcab4d92372a04961a5a76d543
Logan Born <lcborn4@gmail.com>
2017-12-10 08:35:38 -0500

    created the grant api

---
+++ api/controllers/GrantController.js
@@ -0,0 +1,11 @@
+/**
+ * GrantController
+ *
+ * @description :: Server-side logic for managing grants
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
