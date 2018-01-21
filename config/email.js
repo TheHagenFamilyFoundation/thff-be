240baa89a796d1be1918ade60309f196eec4bda5
Logan Born <lcborn4@gmail.com>
2018-01-21 11:19:18 -0500

    added the postmaster settings

---
+++ config/email.js
@@ -0,0 +1,10 @@
+module.exports.email = {
+    service: "Mailgun",
+    auth: {
+      user: "postmaster@hagen.foundation",
+      pass: "064843331d131b03c8a6303e3556fedb"
+    },
+    from: "email@hagen.foundation",
+    templateDir: "views/emailTemplates",
+    testMode: false
+  }
