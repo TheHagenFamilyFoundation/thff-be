930e887d9fb9cecba559ee70aa765be069e5499a
Logan Born <lcborn4@gmail.com>
2018-01-27 16:52:30 -0500

    created the CreateResetCode

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -90,6 +90,75 @@ module.exports = {
 
         });
 
-    }
+    }//end of EmailExists
+    ,
+
+    CreateResetCode: function (req, res) {
+        sails.log("Create Reset Code")
+
+        User.find({
+            email: req.query.email
+        }).exec(function (err, emailfound) {
+            if (err) {
+                return res.json(err.status, { err: err });
+            }
+
+            if (emailfound) {
+
+                if (emailfound.length > 0) {
+                    sails.log("email is found");
+                    //return res.json({ emailfound: true });
+                    //debug
+                    sails.log(emailfound[0].id);
+
+                    var text = "";
+                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                  
+                    for (var i = 0; i < 5; i++)
+                      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                    sails.log(text)
+
+                    User.update(
+                        { id: emailfound[0].id },
+                        {
+                            //debug
+                            resetCode: text,
+                            resetPassword: true
+                        }
+                    )
+                        .exec(function (err, emailfound) {
+
+                            sails.log(emailfound)
+                            res.send(200);
+
+                        })
+
+
+
+                }
+                else {
+                    sails.log("email is not found");
+                    res.send(404);
+                }
+            }//end of emailfound if
+
+        });
+
+
+        //sails.log(req.query.userid);
+
+        //add the 2 new fields to the document
+
+        //create a hashed value for the resetCode
+
+
+
+
+
+
+
+
+    }//end of CreateResetPassword
 
 };
