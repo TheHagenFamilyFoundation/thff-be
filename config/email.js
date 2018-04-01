766227501202822797f2b2688a3e9a50902d6d11
Logan Born <lcborn4@gmail.com>
2018-04-01 15:35:40 -0400

    created the email POST routes

--- config/email.js
+++ config/email.js
@@ -1,10 +1,13 @@
+
+locals = require('./local');
+
 module.exports.email = {
-    service: "Mailgun",
-    auth: {
-      user: process.env.MAILGUN_USER,
-      pass: process.env.MAILGUN_PASSWORD
-    },
-    from: "email@hagen.foundation",
-    templateDir: "views/emailTemplates",
-    testMode: false
-  }
+  service: "Mailgun",
+  auth: {
+    user: process.env.MAILGUN_USER || locals.mailgun_user,
+    pass: process.env.MAILGUN_PASSWORD || locals.mailgun_password
+  },
+  from: "email@hagen.foundation",
+  templateDir: "views/emailTemplates",
+  testMode: false
+}
