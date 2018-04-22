4a84937e2abc27a9e5338473eed32d17ab7a996d
Logan Born <lcborn4@gmail.com>
2018-04-22 14:47:48 -0400

    create function for organization tied with username

--- api/controllers/OrganizationController.js
+++ api/controllers/OrganizationController.js
@@ -6,6 +6,34 @@
  */
 
 module.exports = {
-	
+
+    create: function (req, res) {
+
+        sails.log("organization create");
+
+        sails.log('req.body', req.body)
+
+        let userId = req.body.userid;
+
+        Organization.create(req.body).exec(function (err, org) {
+
+            sails.log("Organization.create")
+
+            if (err) {
+                return res.json(err.status, { err: err });
+            }
+
+            // org is filled with user new data..
+            sails.log("Organization data has been created", org, userId);
+
+            // Adding users to org (userId has a value here);
+            org.users.add(userId);
+
+            // Save
+            org.save(function (err) { console.log('err', err) });
+        });
+
+    }
+
 };
 
