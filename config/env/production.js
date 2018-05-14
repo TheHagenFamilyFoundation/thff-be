bc189f31e88c796ee9e339d86d7cb38d8a1e7e7d
Logan Born <lcborn4@gmail.com>
2018-05-13 21:39:07 -0400

    added the www

--- config/env/production.js
+++ config/env/production.js
@@ -151,7 +151,7 @@ module.exports = {
     ***************************************************************************/
     cors: {
       allowOrigins: [
-        'http://hagen.foundation',
+        'http://www.hagen.foundation', 'https://hagenfoundation.herokuapp.com'
       ]
     },
 
