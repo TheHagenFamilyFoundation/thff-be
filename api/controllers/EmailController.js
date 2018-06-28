837f433716ca2301ac62ac24cc98fd55eda8a734
Logan Born <lcborn4@gmail.com>
2018-06-28 18:46:51 -0400

    new email format implemented

--- api/controllers/EmailController.js
+++ api/controllers/EmailController.js
@@ -7,27 +7,27 @@
 
 module.exports = {
 
-    create: function (req, res) {
+    // create: async function (req, res) {
 
-        sails.log('email create')
+    //     sails.log('email create')
 
-        const email = req.body
-        sails.hooks.email.send(
-            "resetPassword",
-            {
-                Name: email.name,
-            },
-            {
-                to: email.to,
-                subject: "THFF: Reset Password Email"
-            },
-            function (err) {
-                console.log(err || "Mail Sent!");
-            }
-        )
-    },
+    //     const email = req.body
+    //     sails.hooks.email.send(
+    //         "resetPassword",
+    //         {
+    //             Name: email.name,
+    //         },
+    //         {
+    //             to: email.to,
+    //             subject: "THFF: Reset Password Email"
+    //         },
+    //         function (err) {
+    //             console.log(err || "Mail Sent!");
+    //         }
+    //     )
+    // },
 
-    sendResetEmail: function (req, res) {
+    sendResetEmail: async function (req, res) {
 
         sails.log("sendResetEmail")
 
@@ -36,7 +36,6 @@ module.exports = {
         sails.log(req.body);
         sails.log("email.name = " + email.name);
         sails.log("email.resetCode = " + email.resetCode);
-        //sails.log("email.resetTime = " + email.resetTime);
 
         var resetURL = '';
 
@@ -51,71 +50,71 @@ module.exports = {
 
         sails.log(resetURL)
 
-        sails.hooks.email.send(
-            "resetPassword",
-            {
+        await sails.helpers.sendTemplateEmail.with({
+            to: email.to,
+            subject: "THFF: Reset Password Email",
+            template: 'email-reset-password',
+            templateData: {
                 Name: email.name,
                 resetCode: email.resetCode,
                 resetURL: resetURL + '/type-new-password/' + email.name + '/' + email.resetCode
-
             },
-            {
-                to: email.to,
-                subject: "THFF: Reset Password Email"
-            },
-            function (err) {
-                console.log(err || "Mail Sent!");
-            }
-        )
+            layout: false
+        });
 
-        //res.send(200);
+        return res.status(200).json(
+            { message: 'Mail Sent!' })
+
     } //sendResetEmail
 
     ,
-    sendResetEmailConfirmation: function (req, res) {
+    sendResetEmailConfirmation: async function (req, res) {
 
         sails.log("sendResetEmailConfirmation")
 
         sails.log(req.body);
         const email = req.body;
 
-        sails.hooks.email.send(
-            "resetPasswordConfirm",
-            {
+        await sails.helpers.sendTemplateEmail.with({
+            to: email.to,
+            subject: "Your THFF Password has Changed",
+            template: 'email-reset-password-confirmm',
+            templateData: {
                 Name: email.name,
+                //To: email.to
+                // fullName: inputs.fullName,
+                // token: newUserRecord.emailProofToken
             },
-            {
-                to: email.to,
-                subject: "Your THFF Password has Changed"
-            },
-            function (err) {
-                console.log(err || "Mail Sent!");
-            }
-        )
+            layout: false
+        });
+
+        return res.status(200).json(
+            { message: 'Mail Sent!' })
 
     }
     ,
-    sendUserNameEmail: function (req, res) {
+    sendUserNameEmail: async function (req, res) {
 
         sails.log("sendUserNameEmail")
 
         sails.log(req.body);
         const email = req.body;
 
-        sails.hooks.email.send(
-            "username",
-            {
+        await sails.helpers.sendTemplateEmail.with({
+            to: email.to,
+            subject: "Your THFF Username",
+            template: 'email-username',
+            templateData: {
                 Name: email.name,
-                To: email.to
+                //To: email.to
+                // fullName: inputs.fullName,
+                // token: newUserRecord.emailProofToken
             },
-            {
-                to: email.to,
-                subject: "Your THFF Username"
-            },
-            function (err) {
-                console.log(err || "Mail Sent!");
-            }
-        )
+            layout: false
+        });
+
+        return res.status(200).json(
+            { message: 'Mail Sent!' })
 
     },
 
@@ -140,48 +139,31 @@ module.exports = {
             layout: false
         });
 
-
-        // sails.hooks.email.send(
-        //     "registerUserEmail",
-        //     {
-        //         Name: email.name,
-        //         To: email.to
-        //     },
-        //     {
-        //         to: email.to,
-        //         subject: "Thank You For Registering A User Account"
-        //     },
-        //     function (err) {
-        //         console.log(err || "Mail Sent!");
-        //     }
-        // )
-
         return res.status(200).json(
             { message: 'Mail Sent!' })
 
     },
-    sendRegisterOrgEmail: function (req, res) {
+    sendRegisterOrgEmail: async function (req, res) {
 
         sails.log("sendRegisterOrgEmail")
 
         sails.log(req.body);
         const email = req.body;
 
-        sails.hooks.email.send(
-            "registerOrgEmail",
-            {
+        // Send "confirm account" email
+        await sails.helpers.sendTemplateEmail.with({
+            to: email.to,
+            subject: "Thank You For Registering An Org Account",
+            template: 'email-register-org',
+            templateData: {
                 Name: email.name, //username that registered the org
-                OrgName: email.orgName,
-                To: email.to
+                OrgName: email.orgName
             },
-            {
-                to: email.to,
-                subject: "Thank You For Registering A Org Account"
-            },
-            function (err) {
-                console.log(err || "Mail Sent!");
-            }
-        )
+            layout: false
+        });
+
+        return res.status(200).json(
+            { message: 'Mail Sent!' })
 
     }
 
