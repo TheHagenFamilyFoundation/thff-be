a32732dabaa2cd5339af1e52357b7cecd67e6e77
Logan Born <lcborn4@gmail.com>
2018-07-04 23:01:42 -0400

    configured aws-sdk

--- api/controllers/OrganizationController.js
+++ api/controllers/OrganizationController.js
@@ -5,6 +5,17 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+    accessKeyId: sails.config.custom.s3_key,
+    secretAccessKey: sails.config.custom.s3_secret,
+    "region": "sa-east-1"
+})
+
+
+var s3 = new AWS.S3();
+
 module.exports = {
 
     create: function (req, res, next) {
@@ -88,7 +99,60 @@ module.exports = {
                 textParams: req.allParams()
             });
         });
-    }
+    },
+    get501c3: function (req, res) {
 
-};
+        sails.log('getting 501c3 for org', req.params)
+
+        sails.log('key:', sails.config.custom.s3_key, );
+        sails.log('secret:', sails.config.custom.s3_secret)
+
+        Organization.find({
+            organizationID: req.params.orgID
+        }).exec(function (err, orgFound) {
+
+            sails.log('org:', orgFound)
+
+            let orgID = orgFound.id;
+
+            Org501c3.find({
+                organization: orgID
+            }).exec(function (err, org501c3Found) {
+
+                sails.log('501c3:', org501c3Found)
+
+                let fileName = org501c3Found[0].fileName;
+
+                sails.log('fileName:', fileName)
+
+                var params = {
+                    Bucket: sails.config.custom.s3_bucket_name,
+                    Key: fileName
+                };
+
+                s3.getObject(params, function (err, data) {
+                    if (err) {
+                        sails.log(err, err.stack); // an error occurred
+
+                    }
+                    else {
+                        sails.log(data);           // successful response
+
+                        return res.status(200).json({
+                            message: 'Yay'
+                        })
+
+                    }
+
+                })
+
+
+
+            })
+
+        })
+
+
+    }
+}
 
