a7d5f463fca863c9167a625c34cfbef4ffbcaaca
Logan Born <lcborn4@gmail.com>
2018-11-17 17:47:02 -0500

    full proposal item

---
+++ api/models/FullProposalItem.js
@@ -0,0 +1,51 @@
+/**
+ * FullProposalItems.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    fpItemID: {
+      type: 'string'
+    },
+    category: {
+      type: 'string'
+    },
+    amount1: {
+      type: 'number'
+    },
+    amount2: {
+      type: 'number'
+    },
+    amount3: {
+      type: 'number'
+    },
+    total: {
+      type: 'number'
+    },
+    fp: {
+      model: 'FullProposal',
+      unique: true
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
