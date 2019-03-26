d4e000315ba579852aeca4cf32275f8d76e40a93
Logan Born <lcborn4@gmail.com>
2019-03-25 22:54:42 -0400

    sails job output users

--- api/services/SailsitJobServices.js
+++ api/services/SailsitJobServices.js
@@ -4,6 +4,11 @@ module.exports = {
         sails.log.info("=====Job Started=====");
         sails.log.debug(new Date());
         sails.log.info("=====Job Started=====\n\n");
+
+        // User.find({}).then((docs)=>{
+        //     sails.log('docs',docs)
+        // })
+
     }
 
 };
