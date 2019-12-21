84e89f972a1d4b50e1ce707035b5f47d049c03c3
Logan Born <lcborn4@gmail.com>
2019-12-20 23:17:16 -0500

    removed the typescript lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  extends: [
-    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-  ],
   'globals': {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly'
