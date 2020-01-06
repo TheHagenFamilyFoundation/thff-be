77a2ae454b96b7f450c955ab665c8f720fccb4a5
Logan Born <lcborn4@gmail.com>
2020-01-05 23:38:43 -0500

    created getGrantTotals count

--- api/controllers/GrantController.js
+++ api/controllers/GrantController.js
@@ -7,5 +7,32 @@
 
 module.exports = {
 
-};
+  async getGrantTotals(req, res) {
+    // find all grants
+    // total the amount awarded
 
+    const grants = await Grant.find();
+
+    sails.log.info('grants', grants);
+
+    let grantTotal = 0;
+
+    grants.forEach((grant) => {
+      sails.log.info('before grantTotal', grantTotal);
+      sails.log.info('amount', grant.Amount);
+      sails.log.info('typeof amount', typeof grant.Amount);
+      sails.log.info('grant', grant);
+      grantTotal += grant.Amount;
+      sails.log.info('after grantTotal', grantTotal);
+    });
+
+    // debug
+    const message = {
+      message: 'Help',
+      grantTotal,
+    };
+
+    return res.status(200).send(message);
+  },
+
+};
