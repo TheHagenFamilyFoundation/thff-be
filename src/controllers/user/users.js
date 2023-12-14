d3a3bb756575d3efcd3b9fe27800a053d0d091d1
Logan Born <lcborn4@gmail.com>
2023-12-14 16:40:04 -0500

    getUser

--- src/controllers/user/users.js
+++ src/controllers/user/users.js
@@ -1,4 +1,19 @@
+import Logger from '../../utils/logger.js';
+
+import { User } from '../../models/index.js'
+
 export const getUsers = async (req, res) => {
   let message = { data: 'OK' };
   return res.status(200).send(message);
+}
+
+//mongo _id
+export const getUser = async (req, res) => {
+  Logger.info('Inside getUser');
+
+  const { id } = req.query;
+
+  const user = await User.findOne({ _id: id });
+
+  return res.status(200).send(user);
 }
