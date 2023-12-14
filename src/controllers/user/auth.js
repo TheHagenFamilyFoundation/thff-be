ad4c5284cbee82efcbda50205e6e6ed46bbfd8c1
Logan Born <lcborn4@gmail.com>
2023-12-14 16:39:58 -0500

    updated log statements, forgot password, refresh token

--- src/controllers/user/auth.js
+++ src/controllers/user/auth.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt';
 import { validationResult } from "express-validator";
 import jwt from 'jsonwebtoken';
 
+import Config from '../../config/config.js';
 import { User, UserSetting, Token } from '../../models/index.js'
 
 import Logger from "../../utils/logger.js";
@@ -12,7 +13,8 @@ export const login = async (req, res) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    Logger.error(`We have Errors: ${errors.array()}`)
+    return res.status(422).json({ error: errors.array() });
   }
   const { email, password } = req.body;
   Logger.info(`User trying to login... email: ${email}`);
@@ -89,7 +91,7 @@ export const register = async (req, res) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    return res.status(422).json({ error: errors.array() });
   }
 
   try {
@@ -142,7 +144,7 @@ export const confirmUser = async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+      return res.status(422).json({ error: errors.array() });
     }
 
     const { confirmCode } = req.body;
@@ -170,7 +172,7 @@ export const confirmUser = async (req, res) => {
     let message = 'Account Confirmed';
     return res.status(200).send({ message });
   } catch (e) {
-    Logger.error(`Error Confirming User`)
+    Logger.error('Error Confirming User');
     return res
       .status(500)
       .json({ code: "USER003", message: "Error Confirming User" });
@@ -178,4 +180,130 @@ export const confirmUser = async (req, res) => {
 }
 //TODO:
 //refreshAccessToken
+export const refreshAccessToken = async (req, res) => {
+  Logger.info('Inside - refreshAccessToken');
+
+  try {
+    // Get the access token
+    const { accessToken, user } = req.body;
+    const parsedUser = JSON.parse(user);
+
+    const verifiedToken = await jwt.verify(accessToken, process.env.TOKEN_SECRET);
+
+    if (!verifiedToken) {
+      return res.status(401).json({ err: "Invalid Token!" });
+    }
+
+    let settingsQuery = {
+      userID: parsedUser._id,
+    };
+
+    //find the settings
+    let userSettings = await UserSetting.findOne(settingsQuery);
+
+    if (!userSettings) {
+      //create settings
+
+      let defaultSettings = {
+        scheme: "light",
+        userID: parsedUser._id,
+      };
+      // If user logged in successfuly we return user, token, and settings as response
+      userSettings = await UserSetting.create(defaultSettings);
+    }
+
+    const token = await jwt.sign({ accessLevel: user.accessLevel, id: user.id }, process.env.TOKEN_SECRET, { expiresIn: process.env.TOKEN_EXPRATION });
+    //store token in db
+    await Token.create({ userID: user._id, token, token_type: 'verification' });
+
+    //return with user model, token and user settings
+    return res.status(200).send({
+      user,
+      token,
+      userSettings,
+    });
+  } catch (e) {
+    Logger.error('Error Refreshing Token')
+    return res
+      .status(500)
+      .json({ code: "TOK002", message: "Error Refreshing Token" });
+  }
+}
+
 //forgotPassword
+export const forgotPassword = async (req, res) => {
+
+  const { email } = req.body;
+
+  Logger.info(`User forgot password... email: ${email}`);
+
+  try {
+
+    //generate code
+    //find user check
+    let emailFound = await User.find({ email });
+
+    if (!emailFound || emailFound.length < 1) {
+      Logger.error("EMAIL001 - Email not found.");
+      return res
+        .status(400)
+        .json({ code: "EMAIL001", message: "Email not found" });
+    }
+
+    Logger.debug("email is found");
+    // debug
+    Logger.debug(`user id  ${emailFound[0]._id}`);
+    Logger.debug(`emailFound array: ${emailFound}`);
+
+    let user = emailFound[0];
+    Logger.verbose(`user: ${user}`);
+
+    let newCode = generateCode();
+    Logger.info(`newCode ${newCode}`);
+    //get date
+    const now = new Date();
+
+    //can remove
+    Logger.info(`date: ${now}`);
+
+    let updatedUser = await User.updateOne(
+      { _id: user._id },
+      {
+        resetCode: newCode,
+        resetPassword: true,
+        resetTime: now,
+      }
+    );
+
+    Logger.verbose(`user updated ${updatedUser}`);
+
+    Logger.verbose(`user.email = ${user.email}`);
+    Logger.verbose(`user.username =  ${user.username}`);
+    Logger.verbose(`user.resetCode = ${newCode}`);
+
+    //TODO: Send Email
+    let resetURL = Config.feURL + `${Config.feURL}/reset-password?rc=${newCode}`;
+    Logger.info(`resetURL = ${resetURL}`);
+    // await sails.helpers.sendTemplateEmail.with({
+    //   to: user.email,
+    //   subject: "THFF: Reset Password Email",
+    //   template: "email-reset-password",
+    //   templateData: {
+    //     Name: user.email,
+    //     resetCode: newCode,
+    //     resetURL: `${resetURL}/reset-password?rc=${newCode}`,
+    //   },
+    //   layout: false,
+    // });
+
+    let message = `forgot password ${email}`;
+
+    return res.status(200).json(message);
+  }
+  catch (e) {
+    Logger.error('Error Processing Forgot Password')
+    return res
+      .status(500)
+      .json({ code: "TOK002", message: "Error Processing Forgot Password" });
+  }
+};
