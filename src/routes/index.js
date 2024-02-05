95e47946e88fff920310d7f1fa28c4087f712f7d
Logan Born <lcborn4@gmail.com>
2024-02-04 19:26:35 -0500

    allow file upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,20 @@
+import fileUpload from 'express-fileupload';
+
 import HealthController from '../controllers/health.js'
 import AuthRouter from './auth.js'
 
 import AuthnMiddleware from '../middlewares/authn.js'
 
 import UserRouter from './user.js'
+
 import OrganizationRouter from './organization.js'
 import OrganizationInfoRouter from './organization-info.js'
+import Organization501c3Router from './organization-501c3.js'
 
 export default (app) => {
+
+  app.use(fileUpload());
+
   //health
   app.get('/health', HealthController.health);
   app.post('/ping', HealthController.ping);
@@ -25,4 +32,7 @@ export default (app) => {
 
   //organization-info
   app.use('/organization-info', OrganizationInfoRouter);
+
+  //organization 501c3
+  app.use('/organization-501c3', Organization501c3Router);
 }
