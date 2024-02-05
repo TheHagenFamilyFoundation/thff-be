0ff7dffb4fd5916ff953121348ee5911e2bca8dc
Logan Born <lcborn4@gmail.com>
2024-02-04 19:26:29 -0500

    remove debug

--- src/middlewares/authn.js
+++ src/middlewares/authn.js
@@ -4,7 +4,6 @@ const AuthnMiddleware = {
 
   authenticateToken: (req, res, next) => {
     let token = req.headers['authorization']; // Express headers are auto converted to lowercase
-    console.log('token', token);
     if (token) {
       if (token.startsWith('Bearer ')) {
         // Remove Bearer from string
