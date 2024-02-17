46db40d68a3309ea2d55f70a710357a06082dd58
Logan Born <lcborn4@gmail.com>
2024-02-17 14:34:47 -0500

    vote routes

---
+++ src/routes/vote.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import * as VoteController from '../controllers/proposal/votes.js';
+import { validateCreateVote } from '../validators/votes.js'
+
+const router = new Router();
+
+router.post('/',
+  validateCreateVote,
+  VoteController.createVote)
+
+export default router;
