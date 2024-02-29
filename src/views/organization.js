b46045a5f9e80c922443c3e9a407b938d4c58b45
Logan Born <lcborn4@gmail.com>
2024-02-28 21:45:25 -0500

    organization email

---
+++ src/views/organization.js
@@ -0,0 +1,22 @@
+export const registerOrganization = `
+<p>Dear {{ email }},</p>
+<br />
+<p>Thank You for Creating an Organization at THFF</p>
+<br />
+<p>You can access the organization from your user page.</p>
+<br />
+<p>Or from the Organizations button on the toolbar</p>
+<br />
+<p>
+  If you haven't uploaded an 501(c)3, you can do so on the 501(c)3 tab on the
+  organization page.
+</p>
+<br />
+<p>
+  If you have any questions or comments, please
+  <a href="mailto:support@hagen.foundation" target="_top">contact us.</a>
+</p>
+
+Thanks,
+
+<p>THFF TEAM</p>`
