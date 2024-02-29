d6c01d353b0bc202f65e03af43bc925f5dc46a7f
Logan Born <lcborn4@gmail.com>
2024-02-28 21:45:16 -0500

    submitted proposal email

---
+++ src/views/proposal.js
@@ -0,0 +1,16 @@
+export const submittedProposal = `
+<p>Dear {{ email }},</p>
+<br />
+<p>Thank You for Submitting a Proposal</p>
+<p>Proposal Name: {{ projectTitle }}
+</p>
+<p>Per our grant review timeline, the Board will announce all awards in June/July.</p>
+<br />
+<p>
+  If you have any questions or comments, please
+  <a href="mailto:support@hagen.foundation" target="_top">contact us.</a>
+</p>
+
+Thanks,
+
+<p>THFF TEAM</p>`
