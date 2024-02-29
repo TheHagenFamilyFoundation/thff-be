2f588014e166726564965b2bcc0c7a05b1e4adfa
Logan Born <lcborn4@gmail.com>
2024-02-28 21:34:40 -0500

    mailgun config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 import Config from './config.js';
-import mongoose from 'mongoose';
+import { mg } from './mailgun.js';
+import mongoose from './mongoose.js';
 import s3 from './s3.js'
 
 export {
   Config,
+  mg,
   mongoose,
   s3
 }
