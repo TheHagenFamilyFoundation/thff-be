9f128fff515c22d943791d55f8baafca584bf3fa
Logan Born <lcborn4@gmail.com>
2024-06-03 16:46:15 -0400

    adding helmet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 //packages
 import express from 'express';
+import helmet from "helmet";
 import cors from 'cors';
 import bodyParser from 'body-parser';
 //config
@@ -14,6 +15,8 @@ const port = Config.appPort;
 // app.use(bearerToken());
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(helmet())
 
 routes(app);
 
